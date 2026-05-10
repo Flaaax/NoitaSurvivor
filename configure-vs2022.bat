5e34f5881530b426eb0df7605f22e7a49573d656
ggg ggg <2958152232@qq.com>
2026-05-10 23:25:33 +0800

    Migrate Cmake dependencies to vcpkg

--- configure-vs2022.bat
+++ configure-vs2022.bat
@@ -3,9 +3,18 @@ setlocal
 
 if "%VCPKG_ROOT%"=="" set "VCPKG_ROOT=C:\dev\vcpkg"
 
+set "SOURCE_DIR=%~dp0."
 set "BUILD_DIR=%~dp0build"
 
-cmake -S "%~dp0" -B "%BUILD_DIR%" -G "Visual Studio 17 2022" -A x64 ^
-  -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake"
+cmake -S "%SOURCE_DIR%" -B "%BUILD_DIR%" -G "Visual Studio 17 2022" -A x64 ^
+  -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" ^
+  -DVCPKG_MANIFEST_MODE=OFF
+
+if errorlevel 1 (
+  echo.
+  echo CMake configure failed.
+  pause
+  exit /b 1
+)
 
 endlocal
